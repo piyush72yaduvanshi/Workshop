0395086df96625fae4fca8e866deb620f74112d5
piyush72yaduvanshi <piyushyaduvanshi6574@gmail.com>
2026-02-18 09:10:56 +0530

    Work shop Code

--- methods.js
+++ methods.js
@@ -3,7 +3,7 @@ let arr = [1, 2, 3, 4, 5];
 let arr2 = arr.map((num)=>num*5);
 console.log(arr);
 console.log(arr2);
-console.log(arr.length);
+console.log(arr.length)
 //filter
 //slice//splice
 //push
